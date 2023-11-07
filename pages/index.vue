e5f7a12ca37ce8232d81e009120b709f8159bb8f
King-Ponpawit <ponpawit.p@thelivingos.com>
2023-11-07 22:00:25 +0700

    Set config

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="header-1">ATHLETS</h1>
+    </div>
+</template>
+
+<style>
+.header-1 {
+    @apply mobile:text-[3.57143rem] tablet:text-[4.42857rem] desktop:text-[6.42857rem] text-[#E7E7E7];
+}
+</style>
---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from 'tailwindcss';
+
+export default <Partial<Config>>{
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+    ],
+    theme: {
+        extend: {
+            display: ['group-hover'],
+            screens: {
+                mobile: { min: '0px', max: '320px' },
+                tablet: { min: '320px', max: '768px' },
+                desktop: { min: '768px' },
+            },
+            transitionProperty: {
+                width: 'width',
+                height: 'height',
+                margin: 'margin',
+            },
+            boxShadow: {
+                navbar: '0 4px 10px 0px rgba(132, 132, 132, 0.1)',
+                main: '0 4px 20px 0 rgba(132, 132, 132, 0.2)',
+                droptable: '0px 0px 20px 4px rgba(132, 132, 132, 0.07)',
+            },
+            fontFamily: {
+                'roboto': 'Roboto',
+            },
+            colors: {
+            },
+        },
+    },
+    plugins: [],
+}
