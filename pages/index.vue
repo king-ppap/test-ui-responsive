8b6009f9e2ea98a6ab76f9686274c0dea0e2c26f
King-Ponpawit <ponpawit.p@thelivingos.com>
2023-11-12 14:14:59 +0700

    Fix deploy on cloudflare pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,63 @@
 <template>
-    <div>
-        <h1
-            class="header-1 mobile:mt-[20px] mobile:mb-[11px] mobile:px-[18px] tablet:pt-[50px] tablet:pl-[37.890625vw] tablet:pb-[15px] desktop:pt-[100px] desktop:pl-[52.5vw]"
-        >
-            ATHLETS
-        </h1>
-        <div class="mobile:hidden">
+    <ClientOnly>
+        <div>
+            <h1
+                class="header-1 mobile:mt-[20px] mobile:mb-[11px] mobile:px-[18px] tablet:pt-[50px] tablet:pl-[37.890625vw] tablet:pb-[15px] desktop:pt-[100px] desktop:pl-[52.5vw]"
+            >
+                ATHLETS
+            </h1>
+            <div class="mobile:hidden">
+                <AppSvgs
+                    :style="`left: calc(0.37890625 * 100vw - 291px)`"
+                    class="desktop:hidden absolute top-[71px] rigth-[53.515625vw] w-[200px] z-[1]"
+                    :name="'/assets/svg/footballer-graphic-tablet.svg'"
+                />
+                <img
+                    alt="footballer-graphic-desktop"
+                    class="mobile:hidden tablet:hidden absolute w-[678px] top-[50px] right-[55.57291667vw]"
+                    src="~/assets/images/footballer-graphic-desktop.png"
+                />
+                <ContentList :list="listATHLETS" custom-class="tablet:pl-[37.890625vw] desktop:pl-[52.5vw]"></ContentList>
+            </div>
+            <div class="tablet:hidden desktop:hidden w-full flex flex-col items-center">
+                <div class="relative w-[218px] flex justify-center">
+                    <AppSvgs class="w-[200px] z-10" :name="'/assets/svg/footballer-graphic-mobile.svg'" />
+                </div>
+                <div class="bg-[#F5F4F9] pt-[72px] px-[18px] pb-[30px] mt-[-55px]">
+                    <Carousel :list="listATHLETS" />
+                </div>
+            </div>
+        </div>
+        <div class="tablet:relative desktop:relative">
+            <h1
+                class="header-1 mobile:mt-[19px] mobile:mb-[23px] mobile:px-[18px] tablet:pl-[30px] tablet:pt-[50px] tablet:pb-[15px] desktop:pt-[100px] desktop:pb-[20px] desktop:pl-[16.71875vw]"
+            >
+                PLAYERS
+            </h1>
             <AppSvgs
-                :style="`left: calc(0.37890625 * 100vw - 291px)`"
-                class="desktop:hidden absolute top-[71px] rigth-[53.515625vw] w-[200px] z-[1]"
-                :name="'/assets/svg/footballer-graphic-tablet.svg'"
+                class="mobile:hidden desktop:hidden absolute top-[31px] w-[56.90104167vw] ml-[43.09895833vw] z-[1] overflow-hidden"
+                :name="'/assets/svg/basketball-graphic-tablet.svg'"
             />
-            <img
-                alt="footballer-graphic-desktop"
-                class="mobile:hidden tablet:hidden absolute w-[678px] top-[50px] right-[55.57291667vw]"
-                src="~/assets/images/footballer-graphic-desktop.png"
+            <AppSvgs
+                class="mobile:hidden tablet:hidden absolute top-[10px] left-[41.875vw] w-[58.125vw] z-[1] overflow-hidden"
+                :name="'/assets/svg/basketball-graphic-desktop.svg'"
             />
-            <ContentList :list="listATHLETS" custom-class="tablet:pl-[37.890625vw] desktop:pl-[52.5vw]"></ContentList>
-        </div>
-        <div class="tablet:hidden desktop:hidden w-full flex flex-col items-center">
-            <div class="relative w-[218px] flex justify-center">
-                <AppSvgs class="w-[200px] z-10" :name="'/assets/svg/footballer-graphic-mobile.svg'" />
+            <div class="mobile:hidden">
+                <ContentList
+                    :list="listPLAYERS"
+                    custom-class="tablet:pl-[30px] tablet:pr-[39.84375vw] desktop:pl-[16.71875vw] desktop:pr-[45.88541667vw]"
+                ></ContentList>
             </div>
-            <div class="bg-[#F5F4F9] pt-[72px] px-[18px] pb-[30px] mt-[-55px]">
-                <Carousel :list="listATHLETS" />
-            </div>
-        </div>
-    </div>
-    <div class="tablet:relative desktop:relative">
-        <h1
-            class="header-1 mobile:mt-[19px] mobile:mb-[23px] mobile:px-[18px] tablet:pl-[30px] tablet:pt-[50px] tablet:pb-[15px] desktop:pt-[100px] desktop:pb-[20px] desktop:pl-[16.71875vw]"
-        >
-            PLAYERS
-        </h1>
-        <AppSvgs
-            class="mobile:hidden desktop:hidden absolute top-[31px] w-[56.90104167vw] ml-[43.09895833vw] z-[1] overflow-hidden"
-            :name="'/assets/svg/basketball-graphic-tablet.svg'"
-        />
-        <AppSvgs
-            class="mobile:hidden tablet:hidden absolute top-[10px] left-[41.875vw] w-[58.125vw] z-[1] overflow-hidden"
-            :name="'/assets/svg/basketball-graphic-desktop.svg'"
-        />
-        <div class="mobile:hidden">
-            <ContentList
-                :list="listPLAYERS"
-                custom-class="tablet:pl-[30px] tablet:pr-[39.84375vw] desktop:pl-[16.71875vw] desktop:pr-[45.88541667vw]"
-            ></ContentList>
-        </div>
-        <div class="tablet:hidden desktop:hidden w-full flex flex-col items-center">
-            <div class="relative w-[94.375vw] h-[250px] flex justify-center">
-                <AppSvgs class="w-[94.375vw] z-10 overflow-hidden" :name="'/assets/svg/basketball-graphic-mobile.svg'" />
-            </div>
-            <div class="bg-[#F5F4F9] pt-[72px] px-[18px] pb-[30px] mt-[-35px]">
-                <Carousel :list="listPLAYERS" />
+            <div class="tablet:hidden desktop:hidden w-full flex flex-col items-center">
+                <div class="relative w-[94.375vw] h-[250px] flex justify-center">
+                    <AppSvgs class="w-[94.375vw] z-10 overflow-hidden" :name="'/assets/svg/basketball-graphic-mobile.svg'" />
+                </div>
+                <div class="bg-[#F5F4F9] pt-[72px] px-[18px] pb-[30px] mt-[-35px]">
+                    <Carousel :list="listPLAYERS" />
+                </div>
             </div>
         </div>
-    </div>
+    </ClientOnly>
 </template>
 
 <script lang="ts">
